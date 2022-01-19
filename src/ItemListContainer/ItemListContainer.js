4decde443a455f166cf49f5cdf9c48f23e3c361b
Victor Ruiz <ruizevictor@gmail.com>
2022-01-19 00:27:28 -0300

    ItemCounter

--- src/ItemListContainer/ItemListContainer.js
+++ src/ItemListContainer/ItemListContainer.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { ItemCount } from '../components/ItemCount/ItemCount'
-import { PageList } from '../components/List/List'
+import React from "react";
+import { ItemCount } from "../components/ItemCount/ItemCount";
+import { PageList } from "../components/List/List";
 
 export const ItemListContainer = () => {
-    return (
-        <>
-            <PageList greeting ="Landing Page Proyecto React"/>
-            <PageList greeting ="SneakerShop"/>
-            <ItemCount stock = {20} initial = {0} />
-        </>
-    )
-
-}
+  return (
+    <>
+      <PageList greeting="Landing Page Proyecto React" />
+      <PageList greeting="SneakerShop" />
+      <ItemCount stock={20} initial={0} />
+    </>
+  );
+};
