bcfe82b0527d5f9532b45399322f4d46e3382942
Victor Ruiz <ruizevictor@gmail.com>
2022-01-24 19:11:02 -0300

    desafio Map Catalogo

--- src/ItemList/ItemData.js
+++ src/ItemList/ItemData.js
@@ -5,6 +5,8 @@ const productos = [
         description: "If you’re looking for a tough sneaker then you need to buy the new adidas Yeezy Boost 700 V2 Tephra. This Yeezy Boost 700 V2 comes with a tephra upper, tephra midsole, and a tephra sole. These sneakers released in June 2019 and retailed for $300.",
         price: 500,
         stock: 2,
+        img:"https://images.stockx.com/images/adidas-Yeezy-Boost-700-V2-Tephra-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606321264"
+        
 
     },
 
@@ -14,6 +16,7 @@ const productos = [
         description: "In a similar fashion to their 2020’s Azareth release, adidas and Yeezy bring back the favored blue design with the 700 V3 Kyanite in 2021.",
         price: 300,
         stock: 4,
+        img: "https://images.stockx.com/images/adidas-Yeezy-700-V3-Kyanite-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1617895639"
 
     },
 
@@ -23,6 +26,7 @@ const productos = [
         description: "This Yeezy 350 V2 is composed of a cinder Primeknit upper with a tonal side stripe. A cinder Boost cushioned midsole sole and gold outsole completes the design. These sneakers released in March of 2020 and retailed for $220.",
         price: 600,
         stock: 6,
+        img:"https://images.stockx.com/images/adidas-Yeezy-Boost-350-V2-Cinder-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606320313"
 
     }
 ]
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,6 +5,7 @@ import "./Item.css"
 export const Item = ({product}) => {
     return(
         <div className="productosList" >
+            <img clasName="imgCatalogo" src={product.img} alt={product.name} height="200"/>
             <h1>{product.name} </h1>
             <p> {product.description} </p>
             <p> Precio : {product.price} $</p>
